86f27cdd3e24f9ea18f608b0ae231e1963ab3bf9
oltree <o.melex@bk.ru>
2023-04-18 19:35:25 +0300

    Added header and routing

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import Link from 'next/link';
+
+import { useRouter } from 'next/router';
+
+import styles from './Header.module.css';
+
+const Header: FC = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <header className={styles.header}>
+      <Link
+        href="/"
+        className={pathname === '/' ? styles.active : ''}
+      >
+        Home
+      </Link>
+      <Link
+        href="/about"
+        className={pathname === '/about' ? styles.active : ''}
+      >
+        About
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+import Layout from '@/components/layout/Layout';
+
+const About: NextPage = () => {
+  return <Layout>About</Layout>;
+};
+
+export default About;
